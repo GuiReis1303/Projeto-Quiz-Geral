ed2287e5a7dfe21aaa6bfe088a7fadad1135a59d
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-05-18 14:24:26 -0300

    18-05 14:30
    - Adição da página inicio.js, editando o inicio.css

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,4 +1,8 @@
-const Inicio = () => {
+import Nivel from './Nivel';
+
+const Inicio = (props) => {
+
+    const alteraTela = props.alteraTela;
     require('./Inicio.css')
     return( 
         <div className='inicio'>
@@ -21,7 +25,16 @@ const Inicio = () => {
             </div>
 
             <div className='login'>
-                a
+                <h2>Bem-vindo ao Quiz sobre Futebol</h2>
+                <p>
+                    Você está preparado para testar os seus conhecimentos sobre Futebol em 4 níveis diferentes? 
+                    Se sim, coloque seu nome abaixo para conseguir aparecer no rank dependendo de sua pontuação
+                    e clique em começar para escolher um nível. Boa sorte!
+                </p>
+                <div className='formulario'>
+                    <p>Digite seu Nome</p><input/><br/>
+                    <button onClick={() => alteraTela(<Nivel alteraTela={alteraTela}/>)}>Enviar</button>
+                </div>
             </div>
 
             <div className='registrar'>
